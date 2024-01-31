1fa959bb6a7ab3f533dafe0da0d30038121f935b
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2024-01-31 16:11:52 +0100

    Managing Tasks & Understanding Prop Drilling

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTask() {
+function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
   return (
     <div className="flex items-center gap-4">
-      <input type="text" className=" w-64 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className=" text-stone-700 hover:text-slate-950">Add Task</button>
+      <input
+        onChange={handleChange}
+        value={enteredTask}
+        type="text"
+        className=" w-64 px-2 py-1 rounded-sm bg-stone-200"
+      />
+      <button
+        onClick={handleClick}
+        className=" text-stone-700 hover:text-slate-950"
+      >
+        Add Task
+      </button>
     </div>
   );
 }
