b4b49c07d732bdd83b1b6158b5d707fccc90c1d4
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2024-01-31 14:39:26 +0100

    Handling project delete

--- src/App.jsx
+++ src/App.jsx
@@ -52,11 +52,25 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const slectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={slectedProject} />;
+  let content = (
+    <SelectedProject project={slectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
